1aed5dfcba3590a5bccb4b0aae7130896a2a67f9
Subhransu Maji <smaji@cs.umass.edu>
2015-10-09 12:02:12 -0400

    files moved

--- texture_setup.m
+++ texture_setup.m
@@ -1,19 +1,18 @@
 function opts = texture_setup(varargin)
-
-opts.learningRate = 0.001*[...
-                          ones(1,10000), ...
-                      0.1 * ones(1,500), ...
-                     0.01 * ones(1,50000), ...
-                    0.001 * ones(1,20000), ...
-                   0.0001 * ones(1,10000) ] ;
-
+% Options for texture synthesis
 opts.imageSize = [224 224 3];               
 opts.TVbeta = 2;
-opts.momentum  = 0.9;
-opts.lambdaTV = 0;
-opts.beta = 3;
-opts.lambdaL2 = 0;
+opts.lambdaTV = 1e-6;
+opts.beta = 6;
+opts.lambdaL2 = 08e-10;
 opts.textureLayer = {'relu1_1','relu2_1','relu3_1','relu4_1','relu5_1'};
-opts.textureLayerWeights = [1 1 1 1 1];
+opts.textureLayerWeights = [10 1 1 1 1];
+opts.useGPU = true;
 
+% minFunc options (no pesky learning rates)
+opts.minFunc.display = 'iter';
+opts.minFunc.maxIter = 100;
+opts.minFunc.useMex = false;
+
+$ Parse additional options
 opts = vl_argparse(opts, varargin);
