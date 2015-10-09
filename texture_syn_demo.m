9a5e1d34788c32453cdb7b815418a0e2507ba030
Subhransu Maji <smaji@cs.umass.edu>
2015-10-09 15:40:24 -0400

    added demo code++

---
+++ texture_syn_demo.m
@@ -0,0 +1,23 @@
+% Demo for texture synthesis
+% Setup directories
+setup;
+
+% Prepare image
+im = imread('data/textures/food.jpg');
+im = imresize(im, [224 224]);
+
+% Load network
+net = load('data/models/imagenet-vgg-verydeep-16.mat');
+
+% Run texture synthesis
+tic;
+imsyn = texture_syn(im, net);
+toc;
+
+% Display synthesized texture image
+figure(1); clf;
+subplot(1,2,1);imagesc(im); axis image off;
+title('input');
+subplot(1,2,2);imagesc(imsyn); axis image off;
+title('synthesized');
+
