db04614fbd1874dd24fbfa06518979300efd038b
Subhransu Maji <smaji@cs.umass.edu>
2015-10-21 08:59:00 -0400

    load net if needed

--- texture_syn_demo.m
+++ texture_syn_demo.m
@@ -3,11 +3,14 @@
 setup;
 
 % Prepare image
-im = imread('data/textures/BrickSmallBrown0293_12_S.png');
+im = imread('data/textures/Camouflage0011_S.png');
+%im = imread('food.jpg');
 im = imresize(im, [224 224]);
 
 % Load network
-% net = load('data/models/imagenet-vgg-verydeep-16.mat');
+if ~exist('net', 'var')
+    net = load('data/models/imagenet-vgg-verydeep-16.mat');
+end
 
 % Run texture synthesis
 tic;
@@ -19,4 +22,4 @@ figure(1); clf;
 subplot(1,2,1);imagesc(im); axis image off;
 title('input');
 subplot(1,2,2);imagesc(imsyn); axis image off;
-title('synthesized');
+title('synthesized'); set(gca, 'Color', [1 1 1]);
