67e65091fed2305858f0072a1a013b6f338c094d
Subhransu Maji <smaji@cs.umass.edu>
2015-10-20 22:30:37 -0400

    a setup that works

--- texture_setup.m
+++ texture_setup.m
@@ -2,20 +2,21 @@ function opts = texture_setup(varargin)
 % Options for texture synthesis
 opts.imageSize = [224 224 3];
 opts.TVbeta = 2;
-opts.lambdaTV = 1e-5;
+opts.lambdaTV = 1e-6;
 opts.beta = 2;
 opts.lambdaLb = 0;
+opts.textureLayer = {};
+opts.textureLayerWeights = [];
 opts.textureLayer = {'relu1_1','relu2_1', 'relu3_1', 'relu4_1', 'relu5_1'};
 opts.textureLayerWeights = [1 1 1 1 1];
-%opts.textureLayer = {};
-%opts.textureLayerWeights = [];
+%opts.attributeLayer = {};
+%opts.attributeLayerWeights = [];
 opts.attributeLayer = {'relu2_2', 'relu3_3', 'relu4_3', 'relu5_3'};
-opts.attributeLayerWeights = [1 1 1 1]*500;
+opts.attributeLayerWeights = [1 1 1 1]*2000;
 opts.attributeDir = 'data/dtd-weights';
-opts.attributeTarget = 'grid';
-opts.useGPU = true;
-opts.clipPrctile = 1; % Use this to determine the min and max
-                        % [range: 0-100]
+opts.attributeTarget = 'honeycombed';
+opts.useGPU = false;
+opts.clipPrctile = 1; % Use this to determine min/max
 
 % Options for texture initialization
 opts.textureInit = 'rand'; % {'rand', 'quilt'};
@@ -25,7 +26,7 @@ opts.quilt.overlap = 2;
 
 % minFunc options (no pesky learning rates)
 opts.minFunc.display = 'iter';
-opts.minFunc.maxIter = 250;
+opts.minFunc.maxIter = 100;
 opts.minFunc.useMex = false;
 
 % Parse additional options
