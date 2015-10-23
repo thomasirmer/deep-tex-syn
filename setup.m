aaa81ea5a2f5a2d20e62f78177f1c7a1f12f698f
Subhransu Maji <smaji@cs.umass.edu>
2015-10-23 10:55:36 -0400

    add path to exp dir

--- setup.m
+++ setup.m
@@ -1,6 +1,11 @@
 % Setup MatConvNet
 run matconvnet/matlab/vl_setupnn.m;
+
 % Setup VLFEAT
 run vlfeat/toolbox/vl_setup.m;
+
 % Setup minFunc
 addpath(genpath('minFunc/'));
+
+% Setup experiments
+addpath('exp/');
