2d6a27c37ee5eadcc450938b18caa8e1f790c4ae
Subhransu Maji <smaji@cs.umass.edu>
2015-10-30 10:41:00 -0400

    experiment to compare initializations

---
+++ exp/compare_init.m
@@ -0,0 +1,52 @@
+function info = compare_init()
+% Experiment comparing different initialization techniques
+setup;
+
+% Load network
+if ~exist('net', 'var')
+    net = load('data/models/imagenet-vgg-verydeep-16.mat');
+end
+
+im = imread('food.jpg');
+im = imresize(im, [224 224]);
+
+
+% Run texture synthesis
+commonArgs = {'imageSize', [224 224 3], ...
+              'TVbeta', 2, ...
+              'lambdaTV', 1e-6, ...
+              'beta', 2, ...
+              'lambdaLb', 0, ...
+              'contentLayer', {}, ...
+              'contentLayerWeights', [], ...
+              'textureLayer', {'relu1_1','relu2_1', 'relu3_1', 'relu4_1', 'relu5_1'}, ...
+              'textureLayerWeights', [1 1 1 1 1], ...
+              'attributeLayer', {}, ...
+              'attributeLayerWeights', [], ...
+              'maxIter', 10, ...
+              'useGPU', false};
+
+numRuns = 1;
+
+for i = 1:numRuns,
+    fprintf('run %i/%i: random\n', i, numRuns);
+    info.resRand{i} = texture_syn(im, net, ...
+                                  {commonArgs{:}, 'textureInit', 'rand'});
+    fprintf('run %i/%i: quilt\n', i, numRuns);
+    info.resQuilt{i} = texture_syn(im, net, ...
+                                   {commonArgs{:}, 'textureInit', 'quilt'});
+end
+info.commonArgs = commonArgs;
+
+% Do some plotting
+figure; clf;
+for i = 1:numRuns,
+    semilogy(info.resRand{i}.info.trace.fval, 'r-'); hold on;
+    semilogy(info.resQuilt{i}.info.trace.fval, 'b-');
+end
+legend('rand', 'quilt');
+xlabel('iter');
+ylabel('objective');
+title('compare init');
+
+
